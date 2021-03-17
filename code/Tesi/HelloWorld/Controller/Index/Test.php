b9681157174d2c11bf81328e9a96bc52de165314
Roberto D'Agnelli <roberto.dagnelli@gmail.com>
2021-03-18 00:10:56 +0100

    curl sample

--- code/Tesi/HelloWorld/Controller/Index/Test.php
+++ code/Tesi/HelloWorld/Controller/Index/Test.php
@@ -10,12 +10,35 @@ class Test extends \Magento\Framework\App\Action\Action
 		\Magento\Framework\View\Result\PageFactory $pageFactory)
 	{
 		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context); 
+		return parent::__construct($context);
 	}
 
 	public function execute()
 	{
 		echo "Hello World";
+    $out = call();
+    echo "Result: ";
+    echo $out;
 		exit;
 	}
+
+  public function call()
+  {
+    echo "calling...";
+    $ch = curl_init();
+
+    // set url
+    curl_setopt($ch, CURLOPT_URL, "https://www.google.com");
+
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // $output contains the output string
+    $output = curl_exec($ch);
+    echo "output: " . $output;
+    // close curl resource to free up system resources
+    curl_close($ch);
+
+    return $output;
+  }
 }
